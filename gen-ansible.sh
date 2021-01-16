df179eed73553a05eb75a6de937ce4f19b34918a
Robert Pofuk <rpofuk@gmail.com>
2021-01-16 08:47:19 +0100

    [AP-62] Add support for base web iamge
    Change-Id: I09d9337f17c39ba4465dae2c29f170b5d665c093

--- gen-ansible.sh
+++ gen-ansible.sh
@@ -3,14 +3,14 @@
 
 line_number=$(grep -n "# BUILDING" 2-Dockerfile.ansible-img  | sed 's/\([0-9]*\):.*/\1/' | head -1)
 
-rm -f 4-*
+rm -f 5-*
 
-for i in $(ls 3*); do 
+for i in $(ls 4*); do 
   name="${i##*.}"
-  file="4-Dockerfile.${name%-*}-ansible-img"
+  file="5-Dockerfile.${name%-*}-ansible-img"
 
   touch $file
   
-  cp "3-Dockerfile.${i##*.}" $file
+  cp "4-Dockerfile.${i##*.}" $file
   cat 2-Dockerfile.ansible-img | tail +${line_number} >> ${file}; 
 done
