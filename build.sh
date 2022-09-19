4b73011d7de095f898c4388297d43f31a114bd2c
ewp10022 <robert.pofuk@rbinternational.com>
2022-09-19 13:51:01 +0200

    [AP-327] Update docer images to centos
    Change-Id: I1f82157af6fb43cfd201e23996cdadac61add3c7

--- build.sh
+++ build.sh
@@ -2,7 +2,36 @@
 
 echo "Building docker images ${BUILD_ID}"
 
-docker build --progress=plain --build-arg BUILD_ID="${BUILD_ID}" -t base-img:b${BUILD_ID} -f   0-Dockerfile.base .
-docker build --progress=plain --build-arg BUILD_ID="${BUILD_ID}" -t common-img:b${BUILD_ID} -f 1-Dockerfile.common .
-docker build --progress=plain --build-arg BUILD_ID="${BUILD_ID}" -t ansible-img:b${BUILD_ID} -f 2-Dockerfile.ansible .
+export DOCKER_URL=local
+export DOCKER_ORG=local
+
+docker build --progress=plain \
+             --build-arg DOCKER_URL=${DOCKER_URL} \
+             --build-arg DOCKER_ORG=${DOCKER_ORG} \
+	     --build-arg BUILD_ID="${BUILD_ID}" \
+	     -t ${DOCKER_URL}/${DOCKER_ORG}/base-img:b${BUILD_ID} \
+	     -f 0-Dockerfile.base-img .
+
+docker build --progress=plain \
+             --build-arg DOCKER_URL=${DOCKER_URL} \
+             --build-arg DOCKER_ORG=${DOCKER_ORG} \
+	     --build-arg BUILD_ID="${BUILD_ID}" \
+	     -t ${DOCKER_URL}/${DOCKER_ORG}/common-img:b${BUILD_ID} \
+	     -f 1-Dockerfile.common-img .
+
+docker build --progress=plain \
+             --build-arg DOCKER_URL=${DOCKER_URL} \
+             --build-arg DOCKER_ORG=${DOCKER_ORG} \
+	     --build-arg BUILD_ID="${BUILD_ID}" \
+	     -t ${DOCKER_URL}/${DOCKER_ORG}/ansible-img:b${BUILD_ID} \
+	     -f 2-Dockerfile.ansible-img .
+
+docker build --progress=plain \
+             --build-arg DOCKER_URL=${DOCKER_URL} \
+             --build-arg DOCKER_ORG=${DOCKER_ORG} \
+	     --build-arg BUILD_ID="${BUILD_ID}" \
+	     -t ${DOCKER_URL}/${DOCKER_ORG}/web-img:b${BUILD_ID} \
+	     -f 3-Dockerfile.web-img .
+
+#docker build --progress=plain --build-arg BUILD_ID="${BUILD_ID}" -t ansible-img:b${BUILD_ID} -f 2-Dockerfile.ansible .
 
