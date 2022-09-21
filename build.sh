cbeaeedff96eaa3ca588f39de0937253a86a78a4
ewp10022 <robert.pofuk@alpha-prosoft.com>
2022-09-21 14:06:51 +0200

    [AP-334] Fix ginallly GIDs
    Change-Id: If88e2b5f9237b7fdf577a859ba8c691cb2cbef5a

--- build.sh
+++ build.sh
@@ -9,29 +9,29 @@ docker build --progress=plain \
              --build-arg DOCKER_URL=${DOCKER_URL} \
              --build-arg DOCKER_ORG=${DOCKER_ORG} \
 	     --build-arg BUILD_ID="${BUILD_ID}" \
-	     -t ${DOCKER_URL}/${DOCKER_ORG}/base-img:b${BUILD_ID} \
+	     -t ${DOCKER_URL}/${DOCKER_ORG}/base-img:local-build \
 	     -f 0-Dockerfile.base-img .
 
 docker build --progress=plain \
              --build-arg DOCKER_URL=${DOCKER_URL} \
              --build-arg DOCKER_ORG=${DOCKER_ORG} \
 	     --build-arg BUILD_ID="${BUILD_ID}" \
-	     -t ${DOCKER_URL}/${DOCKER_ORG}/common-img:b${BUILD_ID} \
+	     -t ${DOCKER_URL}/${DOCKER_ORG}/common-img:local-build \
 	     -f 1-Dockerfile.common-img .
 
 docker build --progress=plain \
              --build-arg DOCKER_URL=${DOCKER_URL} \
              --build-arg DOCKER_ORG=${DOCKER_ORG} \
 	     --build-arg BUILD_ID="${BUILD_ID}" \
-	     -t ${DOCKER_URL}/${DOCKER_ORG}/ansible-img:b${BUILD_ID} \
+	     -t ${DOCKER_URL}/${DOCKER_ORG}/ansible-img:local-build \
 	     -f 2-Dockerfile.ansible-img .
 
 docker build --progress=plain \
              --build-arg DOCKER_URL=${DOCKER_URL} \
              --build-arg DOCKER_ORG=${DOCKER_ORG} \
 	     --build-arg BUILD_ID="${BUILD_ID}" \
-	     -t ${DOCKER_URL}/${DOCKER_ORG}/web-img:b${BUILD_ID} \
+	     -t ${DOCKER_URL}/${DOCKER_ORG}/web-img:local-build \
 	     -f 3-Dockerfile.web-img .
 
-#docker build --progress=plain --build-arg BUILD_ID="${BUILD_ID}" -t ansible-img:b${BUILD_ID} -f 2-Dockerfile.ansible .
+#docker build --progress=plain --build-arg BUILD_ID="${BUILD_ID}" -t ansible-img:local-build -f 2-Dockerfile.ansible .
 
